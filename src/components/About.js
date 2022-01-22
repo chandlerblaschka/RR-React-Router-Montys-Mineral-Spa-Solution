3655fe1fa9fc0b9e61338c3e6cc8a763b015d58f
chandlerblaschka <cblaschka@live.com>
2022-01-22 14:32:46 -0600

    finished

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container'
 export default function About() {
     return (
         <div className="aboutpage">
+            {/* <div className='aboutHeader'>
+                <h1>About Us</h1>
+            </div> */}
             <Container>
                 <Card border="info" >
                     <Card.Header className="blockquote mb-0 card-body">About Us</Card.Header>
